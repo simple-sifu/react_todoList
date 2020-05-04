dc9b948a8793adf7c5b7bc6d4044c1f86cd88414
Tommy Han <tommy@Tommys-Air.home>
2020-05-04 15:13:22 -0400

    setup webpack dev server

--- client/components/GameList.js
+++ client/components/GameList.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GameList = (props) => {
+const GameList = props => {
   const gameList = props.gameList;
   const listElements = gameList.map(game => (
-    <li key={game.createdAt}>{game.winner} won at {game.createdAt}</li>
+    <li key={game.createdAt}>
+      {game.winner} won at {game.createdAt}
+    </li>
   ));
   return (
     <div id="gameList">
-      <h3>Previous matches</h3>
-      <ul>
-        {listElements}
-      </ul>
+      <h3>Past matches:</h3>
+      <ul>{listElements}</ul>
     </div>
   );
 };
--- client/index.js
+++ client/index.js
@@ -3,9 +3,6 @@ import { render } from 'react-dom';
 import App from './components/App';
 
 // uncomment so that webpack can bundle styles
-// import styles from './scss/application.scss';
+import styles from './scss/application.scss';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
